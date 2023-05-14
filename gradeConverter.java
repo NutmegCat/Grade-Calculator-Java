9bb5261707c2212cb41448c1e3dbc0ce1f4ca5e7
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-05-13 20:32:19 -0400

    updated wording and .gitignore

--- gradeConverter.java
+++ gradeConverter.java
@@ -14,22 +14,22 @@ class gradeConverter{
         
         //ask for current grade
         System.out.println("\nWhat is your current grade?");
-        converToGrade(In.getInt());
+        convertToGrade(In.getInt());
     }
 
-    public static void converToGrade(int usermark) {
+    public static void convertToGrade(int usermark) {
         
         //grade range into character
         if (80 <= usermark && usermark <= 100) {
-            System.out.println("\nYou have an: " + A + "\n");
+            System.out.println("\nYou have a: " + A + "\n");
         } else if (70 <= usermark && usermark <= 79){
-            System.out.println("\nYou have an: " + B + "\n");
+            System.out.println("\nYou have a: " + B + "\n");
         } else if (60 <= usermark && usermark <= 69) {
-            System.out.println("\nYou have an: " + C + "\n");
+            System.out.println("\nYou have a: " + C + "\n");
         } else if (50 <= usermark && usermark <= 59) {
-            System.out.println("\nYou have an: " + D + "\n");
+            System.out.println("\nYou have a: " + D + "\n");
         } else if (0 <= usermark && usermark <= 49) {
-            System.out.println("\nYou have an: " + F + "\n");
+            System.out.println("\nYou have a: " + F + "\n");
         } else {
             System.out.println("\nYou have an: " + X + "\n");
         }
